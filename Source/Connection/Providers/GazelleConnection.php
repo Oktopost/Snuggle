e7286088ab1509201a751c64f9db91fb4e9ab87f
Alexey Puchkov <alexey.pkv@gmail.com>
2021-03-09 12:45:54 +0200

    Add Gazelle connector

---
+++ Source/Connection/Providers/GazelleConnection.php
@@ -0,0 +1,81 @@
+<?php
+namespace Snuggle\Connection\Providers;
+
+
+use Gazelle\Exceptions\ResponseException;
+use Gazelle\Gazelle;
+use Gazelle\IResponse;
+use Gazelle\Exceptions\RequestException;
+
+use Snuggle\Base\IConnection;
+use Snuggle\Base\Connection\Request\IRawRequest;
+use Snuggle\Base\Connection\Response\IRawResponse;
+
+use Snuggle\Connection\Response\RawResponse;
+use Snuggle\Exceptions\SnuggleException;
+use Snuggle\Exceptions\ServerUnreachableException;
+use Snuggle\Connection\Request\RawRequest;
+
+
+class GazelleConnection implements IConnection
+{
+	private Gazelle $gazelle;
+	
+	
+	private function send(IRawRequest $request): IResponse
+	{
+		$gazelleRequest = $this->gazelle->request()
+			->addPath($request->getURI())
+			->setQueryParams($request->getQueryParams())
+			->setMethod($request->getMethod())
+			->setHeaders($request->getHeaders());
+		
+		if ($request->hasBody())
+		{
+			$gazelleRequest->setBody($request->getBody());
+		}
+		
+		return $gazelleRequest->send();
+	}
+	
+	private function parse(IRawRequest $request, IResponse $response): IRawResponse
+	{
+		return new RawResponse(
+			$request,
+			$response->getCode(), 
+			$response->getHeaders(),
+			$response->hasBody() ? $response->getBody() : null
+		);
+	}
+	
+	
+	public function __construct(Gazelle $gazelle)
+	{
+		$this->gazelle = $gazelle;
+	}
+
+
+	public function request($request, string $method = '', array $params = []): IRawResponse
+	{
+		$request = RawRequest::toRequest($request, $method, $params);
+		
+		try
+		{
+			$response = $this->send($request);
+		}
+		catch (ResponseException $resE)
+		{
+			$response = $resE->response();
+		}
+		catch (RequestException $reqE)
+		{
+			throw new ServerUnreachableException($reqE->getMessage(), $reqE->getCode(), $reqE);
+		}
+		catch (\Exception $e)
+		{
+			throw new SnuggleException($e->getMessage(), $e->getCode(), $e);
+		}
+		
+		return $this->parse($request, $response);
+	}
+}
---
+++ Source/Factories/Connections/GazelleConnectionFactory.php
@@ -0,0 +1,85 @@
+<?php
+namespace Snuggle\Factories\Connections;
+
+
+use Gazelle\Gazelle;
+use Snuggle\Base\Factories\IConnectionFactory;
+use Snuggle\Base\IConnection;
+use Snuggle\Config\ConnectionConfig;
+use Snuggle\Connection\Providers\GazelleConnection;
+
+
+class GazelleConnectionFactory implements IConnectionFactory
+{
+	private $configCallback;
+	private array $connections = [];
+	
+	
+	private function store(Gazelle $gazelle, string $key): void
+	{
+		if (count($this->connections) > 32)
+		{
+			unset($this->connections[random_int(0, count($this->connections) - 1)]);
+			$this->connections = array_values($this->connections);
+		}
+		
+		$this->connections[$key] = $gazelle;
+	}
+	
+	private function getKey(ConnectionConfig $config): string
+	{
+		return sha1(jsonencode($config->toArray()));
+	}
+	
+	private function createConnectorForConfig(ConnectionConfig $config): Gazelle
+	{
+		$gazelle = new Gazelle();
+		$this->configGazelle($gazelle, $config);
+		return $gazelle;
+	}
+	
+	
+	protected function configGazelle(Gazelle $gazelle, ConnectionConfig $config): void
+	{
+		$template = $gazelle
+			->template()
+			->setURL($config->getURL());
+			
+		if ($config->hasCredentials())
+		{
+			$template->setCurlOption(
+				CURLOPT_USERPWD, 
+				"{$config->User}:{$config->Password}");
+		}
+		
+		if ($this->configCallback)
+		{
+			$callback = $this->configCallback;
+			$callback($gazelle, $config);
+		}
+	}
+	
+	
+	public function __construct(?callable $configCallback = null)
+	{
+		$this->configCallback = $configCallback;
+	}
+
+
+	public function get(ConnectionConfig $config): IConnection
+	{
+		$key = $this->getKey($config);
+		
+		if (isset($this->connections[$key]))
+		{
+			$gazelle = $this->connections[$key];
+		}
+		else
+		{
+			$gazelle = $this->createConnectorForConfig($config);
+			$this->store($gazelle, $key);
+		}
+		
+		return new GazelleConnection($gazelle);
+	}
+}
